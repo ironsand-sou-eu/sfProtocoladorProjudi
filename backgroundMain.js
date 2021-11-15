51bca60b0b7e9c95bc8ccfb69d712b58963338f6
César Rodriguez <54708668+ironsand-sou-eu@users.noreply.github.com>
2021-11-15 00:22:33 -0300

    Remoção de permissões desnecessárias

--- backgroundMain.js
+++ backgroundMain.js
@@ -1,25 +1,7 @@
-// Regra para exibir botão
-var regra1 = {
-  conditions: [
-    new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'projudi.tjba.jus.br' },
-    })
-  ],
-  actions: [ new chrome.declarativeContent.ShowPageAction() ]
-};
-
-chrome.runtime.onInstalled.addListener(function(details) {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([regra1]);
-  });
-});
-
 // Listener para clique na action
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    const details = { tabId: tabs[0].id };
+chrome.action.onClicked.addListener(tab => {
+    const details = { tabId: tab.id };
     sendMessage(details);
-  });
 });
 
 // Listener para navegação
